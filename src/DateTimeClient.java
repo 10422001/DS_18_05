599db062f2a20bb50c5bc12dab27ef5bf656be7a
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-05-18 14:42:03 +0700

    add DatetimeClient

---
+++ src/DateTimeClient.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.net.Socket;
+import java.io.IOException;
+public class DateTimeClient {
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Pass the server IP as the sole command line argument");
+            return;
+        }
+        Socket socket = new Socket(args[0], 5000);
+        Scanner in = new Scanner(socket.getInputStream());
+        System.out.println("Server response: " + in.nextLine());
+    }
+}
