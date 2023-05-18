8b3ed4b17b51b550df205c80a9b845d572dc9f4b
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-05-18 14:39:04 +0700

    add Datetimeserver

---
+++ src/DateTimeServer.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+public class DateTimeServer {
+
+    public final static int daytimePort = 5000;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket theServer;
+        Socket theConnection;
+        PrintStream p;
+
+        try {
+            theServer = new ServerSocket(daytimePort);
+            while (true) {
+                theConnection = theServer.accept();
+                p = new PrintStream(theConnection.getOutputStream());
+//                Date now = new Date();
+                p.println(new Date());
+                theConnection.close();
+                theServer.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+}
