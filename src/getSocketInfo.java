337e0b9585fb1cc6aecb324c23b18a282e393370
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-05-18 14:00:53 +0700

    not complete

--- src/getSocketInfo.java
+++ src/getSocketInfo.java
@@ -5,19 +5,25 @@ import java.io.*;
 public class getSocketInfo {
 
     public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("www.google.com", 80);
-            System.out.println("Connected to " + socket.getInetAddress() + " on port " + socket.getPort() + " from port " + socket.getLocalPort() + " of " + socket.getLocalAddress());
-            socket.close();
-        } catch (UnknownHostException e) {
-            System.err.println("I can't find " + e);
-        } catch (SocketException e) {
-            System.err.println("Could not connect to " + e);
-        } catch (IOException e) {
-            System.err.println(e);
+
+        for (int i = 0; i < args.length; i++) {
+            try {
+                Socket theSocket = new Socket(args[i], 80);
+                System.out.println("Connected to " + theSocket.getInetAddress()
+                        + " on port " + theSocket.getPort() + " from port "
+                        + theSocket.getLocalPort() + " of "
+                        + theSocket.getLocalAddress());
+            } catch (UnknownHostException e) {
+                System.err.println("I can't find " + args[i]);
+            } catch (SocketException e) {
+                System.err.println("Could not connect to " + e);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
         }
+
     }
-
-
-
 }
+
+
+
